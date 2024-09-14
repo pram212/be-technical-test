50867c28b3020d96542f1dc9fb5f8e8e82702bb0
pramono <pramono6236@gmail.com>
2024-09-15 00:38:50 +0700

    feat: :globe_with_meridians: setup endpoint API

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,8 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->statefulApi();
+        $middleware->validateCsrfTokens(except:['/authors', '/authors/*', '/books', '/books/*']);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        
     })->create();
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::apiResource('authors', AuthorController::class);
+Route::apiResource('books', BookController::class);
+Route::get('authors/{id}/books', [ BookController::class, 'getBookByAuthor' ])->name('books.by.author');
+
