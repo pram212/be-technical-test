16158b6c0678e10ab9f5ed0b2fabe60a6c65253c
pramono <pramono6236@gmail.com>
2024-09-15 12:44:32 +0700

    perf: :zap: improving performance with indexing database

---
+++ database/migrations/2024_09_15_052611_add_indexing_to_books_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('books', function (Blueprint $table) {
+            $table->index('title');
+            $table->index('publish_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('books', function (Blueprint $table) {
+            $table->dropIndex('books_title_index');
+            $table->dropIndex('books_publish_date_index');
+        });
+    }
+};
